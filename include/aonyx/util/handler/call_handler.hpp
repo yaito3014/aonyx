59d5a1a9117940d86c536253aea30072912774a1
Ogame3334 <kazuhiroisobe0522@gmail.com>
2026-06-16 12:19:50 +0900

    refactor: improve formatting and readability in call_handler implementation

--- include/aonyx/util/handler/call_handler.hpp
+++ include/aonyx/util/handler/call_handler.hpp
@@ -15,19 +15,20 @@ namespace aonyx
 {
     namespace util
     {
-        namespace details{
+        namespace details
+        {
             template <class Fn, class... Args>
             void call_handler_impl(Fn fn, const http::request &req, http::response &res, Args... args)
             {
                 return fn(req, res, args...);
             }
-    
+
             template <class Fn, std::size_t... I>
             bool call_handler(Fn fn, const http::request &req, http::response &res, const inner_handler_params_t params, std::index_sequence<I...>)
             {
                 call_handler_impl<
-                Fn,
-                std::tuple_element_t<I, handler_args_tuple_t<Fn>>...>(
+                    Fn,
+                    std::tuple_element_t<I, handler_args_tuple_t<Fn>>...>(
                     fn,
                     req,
                     res,
@@ -35,13 +36,14 @@ namespace aonyx
                         std::tuple_element_t<I, handler_args_tuple_t<Fn>>>(params[I]))...);
                 return true;
             }
-    
+
             template <class Fn>
             bool call_handler(Fn fn, const http::request &req, http::response &res, const inner_handler_params_t params)
             {
                 auto fn_args_size = util::handler_args_size_v<Fn>;
 
-                if(fn_args_size != params.size()){
+                if (fn_args_size != params.size())
+                {
                     return false;
                 }
 
@@ -50,7 +52,7 @@ namespace aonyx
                     req,
                     res,
                     params,
-                    std::make_index_sequence<std::tuple_size<handler_args_tuple_t<Fn>>::value>{});
+                    std::make_index_sequence<handler_args_size_v<Fn>>{});
             }
         }
     }
